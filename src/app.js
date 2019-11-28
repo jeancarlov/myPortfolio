6416f577c9061c7ec11fe1788088760139b1f0ea
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-28 11:33:30 -0500

    state components usong the bio variable before is refactor to clearner code

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 
-class RegularClass {}
-class ComponentClass extends Component{}
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-// log the instance , and the actual object itself
-console.log('regularClassInstance', regularClassInstance);
-console.log('componentClassInstance', componentClassInstance);
 
 
 class App extends Component {
-    render(){
-        return(
+    constructor(){
+    super();
+       this.state = { displayBio: false }; 
+    }
+
+    render() {
+        let bio = (
             <div>
+                <p>I live in greater Orlando Area, and code almost everyday.</p>
+                <p> My favorite language so far is python but I am currently reactiving my love for ECMASript</p>
+                <p> Besides coding, I love reading and working out for wellbeing</p>
+            </div>
+        );
+
+        if (!this.state.displayBio) {
+            bio = null;
+        }
+
+        return(
+         <div>
                 <h1> Hello!</h1>
                 <p>My name is Jean Carlo Valderrama</p> 
                 <p> I enjoy coding as a habit</p>
-            </div>
+                {bio} 
+        </div>  
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,44 +4,4 @@ import App from './app';
 
 ReactDOM.render(<App></App>, document.getElementById('root'));
 
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    speak(){
-        console.log('I am', this.name, 'and I am', this.age, 'years old');
-        
-    }
-}
-
-const animal1 = new Animal('simba',3);
-animal1.speak();
-
-console.log(animal1);
-
-// note that the constructor needs to have the same parameters as the original class (name, age) I can declare new parameters for this class
-class Lion extends Animal{
-    constructor(name, age, furColor, speed){
-        super(name, age);
-        this.furColor= furColor;
-        this.speed= speed;
-    }
-
-    roar(){
-        console.log(
-            'ROOAR',
-            this.furColor,
-            this.speed,
-            'miles an hour!'
-        );
-        
-    }
-}
-
-const lion1 = new Lion ('mufasa', 20, 'golden', 30);
-lion1.speak();
-lion1.roar();
-console.log(lion1);
 
