d6efdae623e244e3d0459bac4b4ef562f2e8d786
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-28 11:44:59 -0500

    bio state component refactor, note that the constructor is use to attach the state object and the super is to use to recall the inheritance of the react component that is extended in the class App

--- src/app.js
+++ src/app.js
@@ -9,24 +9,21 @@ class App extends Component {
     }
 
     render() {
-        let bio = (
-            <div>
-                <p>I live in greater Orlando Area, and code almost everyday.</p>
-                <p> My favorite language so far is python but I am currently reactiving my love for ECMASript</p>
-                <p> Besides coding, I love reading and working out for wellbeing</p>
-            </div>
-        );
-
-        if (!this.state.displayBio) {
-            bio = null;
-        }
 
         return(
          <div>
                 <h1> Hello!</h1>
                 <p>My name is Jean Carlo Valderrama</p> 
                 <p> I enjoy coding as a habit</p>
-                {bio} 
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in greater Orlando Area, and code almost everyday.</p>
+                            <p> My favorite language so far is python but I am currently reactiving my love for ECMASript</p>
+                            <p> Besides coding, I love reading and working out for wellbeing</p>
+                        </div>
+                    ) : null
+                }
         </div>  
         )
     }
