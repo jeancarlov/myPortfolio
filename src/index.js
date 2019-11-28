ad7ced59161909f72ffb9da59abe08309ba2fdc6
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-28 10:27:03 -0500

    insights on class, inheritance, and components work on react and javascript

--- src/index.js
+++ src/index.js
@@ -20,3 +20,28 @@ const animal1 = new Animal('simba',3);
 animal1.speak();
 
 console.log(animal1);
+
+// note that the constructor needs to have the same parameters as the original class (name, age) I can declare new parameters for this class
+class Lion extends Animal{
+    constructor(name, age, furColor, speed){
+        super(name, age);
+        this.furColor= furColor;
+        this.speed= speed;
+    }
+
+    roar(){
+        console.log(
+            'ROOAR',
+            this.furColor,
+            this.speed,
+            'miles an hour!'
+        );
+        
+    }
+}
+
+const lion1 = new Lion ('mufasa', 20, 'golden', 30);
+lion1.speak();
+lion1.roar();
+console.log(lion1);
+
