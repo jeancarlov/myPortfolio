721eff09f074ef6e25af9d59ded029b552bcf57a
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-29 13:17:38 -0500

    js destructuring apply to get the this.props.title image description and link. site is working properly

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+// Use js destructuring to access the this.props.project.title,description, image for code efficiency
 class Project extends Component{
     render() {
         console.log('this.props', this.props);
+        const { title, image, description, link } =this.props.project;
+
         return (
             <div>
-                {this.props.project.title}
+                <h3>{title}</h3>
+                <img src={image} alt= 'profile'/>
+                <p>{description}</p>
+                <a href = {link}>{link}</a>
             </div>
         )
     }
