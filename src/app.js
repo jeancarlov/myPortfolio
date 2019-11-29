3cabcada2c8fa79eacee0599881604e548fea262
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-29 10:20:16 -0500

    this.readMore method is within the constructor but its going to be refactor to a class for code efficiency

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
-
+// add a contructor key method and it gets fire when an instance of a class is create
+// the constructor is use to attach a state object to the component using the (this)
+// if the state object is undefined use the (super() ) to invoke the parent classes of the constructor method 
+// state is add to control the display of bio
+// change the null value  for jsx
+// us ethe setStaste to update the values of a state
 
 class App extends Component {
     constructor(){
     super();
        this.state = { displayBio: false }; 
+       // console.log('component this', this);
+       
+       this.readMore = ()=> {
+        // console.log('readMore this', this);
+        
+        this.setState({ displayBio: true});
+       }
     }
-
     render() {
 
         return(
@@ -22,7 +33,11 @@ class App extends Component {
                             <p> My favorite language so far is python but I am currently reactiving my love for ECMASript</p>
                             <p> Besides coding, I love reading and working out for wellbeing</p>
                         </div>
-                    ) : null
+                    ) : (
+                        <div>
+                            <button on onClick={this.readMore}> Read more</button>
+                        </div>
+                    )
                 }
         </div>  
         )
