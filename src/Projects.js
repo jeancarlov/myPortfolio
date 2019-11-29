ff3c029bb4c9c9b4293540fd1f1ebdf5b88264c9
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-29 12:59:54 -0500

    props added to the project to pass parent components to the child components before destructuring from js for code efficiency

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component{
+    render() {
+        console.log('this.props', this.props);
+        return (
+            <div>
+                {this.props.project.title}
+            </div>
+        )
+    }
+}
+ // Note: Props is a way for parent components to pass(data) to child components
+ // Creat a key attribute to pass PROJECT.id and project attribute to pass the entire PROJECT index from the map(PROJECT)
+ // notice that the project attribute is the same as the value from this.props.project.title because its the way props work
 class Projects extends Component {
     render(){
         return(
@@ -10,7 +23,7 @@ class Projects extends Component {
                     {
                         PROJECTS.map((PROJECT) => {
                             return(
-                                <div key={PROJECT.id}>{PROJECT.title}</div>
+                               <Project key={PROJECT.id} project={PROJECT} />
                             );
                         })
                     }
