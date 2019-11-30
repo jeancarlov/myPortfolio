cd1b6f65bc1d4845ab388331e133b81ee127e795
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-29 20:52:03 -0500

    componentDidMount is working but i need to fix when the elements  runsout in the index by using the modulu operator

--- src/Title.js
+++ src/Title.js
@@ -12,6 +12,19 @@ const TITLES = [
 class Title extends Component{
     state = { titleIndex : 0 };
 
+    //normal class method syntax 
+    componentDidMount(){
+        console.log('title component has mounted');
+
+        this.animateTiles();
+    }
+    animateTiles= () => {
+        setInterval (() => {
+            const titleIndex = this.state.titleIndex + 1;
+
+            this.setState({ titleIndex });
+        }, 4000);
+    }
     render(){
         const title = TITLES[this.state.titleIndex];
 
