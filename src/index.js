57daa7dedba829e18cebb79f4ebcbd3fb1c3bf3e
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-30 19:51:25 -0500

    Router added to portfolio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
+const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={history}>
+        <Switch>
+            <Route path='/' component={App} />
+        </Switch>
+    </Router>,
+     document.getElementById('root')
+     );
 
 
