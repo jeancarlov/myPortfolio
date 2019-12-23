1dc3b6636981a2dc85ddffa6a8a6d4e27ef84227
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-12-22 19:24:24 -0500

    fetching weather data response is working but data is not displaying, currently fixing bud

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,42 @@
 import React, { Component} from 'react';
 
 class Weather extends Component {
-    state = { weather: {}}
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          cities: []
+        };
+      }
 
     componentDidMount(){
-        fetch('api.openweathermap.org/data/2.5/weather?q=Orlando,US&appid={key}a&units=imperial')
-        .then((response) => console.log('response', response));
-    }
+        fetch('api.openweathermap.org/data/2.5/weather?q=Orlando,US&appid=d3fb420c795be29b9317c50947d0c70a&units=imperial')
+        .then(res => console.log('response', res)
+        )
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+           
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+    
 
     render() {
         return(
-            <div></div>
+            <div>Cities</div>
         )
     }
 }
