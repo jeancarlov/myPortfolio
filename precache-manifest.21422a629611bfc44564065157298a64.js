399f04c7a428cf1b971d43d1f17ae5b5fc88159c
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2020-01-26 18:45:57 -0500

    Updates

---
+++ precache-manifest.21422a629611bfc44564065157298a64.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5648a7595d03c78162ac7e40b09ea344",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "20a2c7ee369db152b0c1",
+    "url": "/myPortfolio/static/css/main.99ca55c4.chunk.css"
+  },
+  {
+    "revision": "18807e0cf959b7eee132",
+    "url": "/myPortfolio/static/js/2.aa314856.chunk.js"
+  },
+  {
+    "revision": "20a2c7ee369db152b0c1",
+    "url": "/myPortfolio/static/js/main.093b27a7.chunk.js"
+  },
+  {
+    "revision": "a4d0b95d35e99d24192c",
+    "url": "/myPortfolio/static/js/runtime-main.360ad822.js"
+  },
+  {
+    "revision": "fe35a1cb170716281a3c1b8827a0809a",
+    "url": "/myPortfolio/static/media/Cristal-Collector-Game-Image.fe35a1cb.png"
+  },
+  {
+    "revision": "1689f564d161c6cc530cdcdd1cff5df3",
+    "url": "/myPortfolio/static/media/My React-Redux Porfolio-Image.1689f564.png"
+  },
+  {
+    "revision": "a045d811fa66fb07a83a495d90677a54",
+    "url": "/myPortfolio/static/media/Project-Python-TravisCI-Image.a045d811.png"
+  },
+  {
+    "revision": "4091ecbe5921fb934d59df2ec050c196",
+    "url": "/myPortfolio/static/media/SiteTDD-Docker-Image.4091ecbe.png"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/myPortfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/myPortfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/myPortfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "ad6a7cb69922f8d13abc92d9b7c221b3",
+    "url": "/myPortfolio/static/media/jcvpic.ad6a7cb6.jpg"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/myPortfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/myPortfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
